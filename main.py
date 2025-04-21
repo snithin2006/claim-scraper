ca1f00178696b44b0c9c3b634236d7c4d4b14087
Nithin Sivakumar <nithin06.siva@gmail.com>
2025-04-21 01:59:29 -0500

    fix

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-from fastapi.middleware.wsgi import WSGIMiddleware
-from scrape_claim_details import scrape_claim_details
 from pydantic import BaseModel
+from scrape_claim_details import scrape_claim_details
 
 app = FastAPI()
 
 class ClaimRequest(BaseModel):
     url: str
 
-@app.post("/scrape")
+@app.post("/scrape")  # This MUST be a POST endpoint
 def scrape(claim: ClaimRequest):
     return scrape_claim_details(claim.url)
